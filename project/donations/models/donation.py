ced4d531841a25fe03641077afa644a6e2b822f3
Gabriel Lins <nombregag@gmail.com>
2020-03-31 18:57:56 -0300

    Added fields delivered and picked for donations

--- project/donations/models/donation.py
+++ project/donations/models/donation.py
@@ -6,6 +6,8 @@ class Donation(models.Model):
     giver = models.ForeignKey(Giver, on_delete=models.CASCADE)
     receiver = models.ForeignKey(Receiver, on_delete=models.CASCADE, blank=True, null=True)
     items = models.TextField(max_length=256)
+    picked = models.BooleanField(default=False)
+    delivered = models.BooleanField(default=False)
 
     def __str__(self):
         return '{} - {}'.format(self.giver, self.items)
