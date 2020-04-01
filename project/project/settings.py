c843392efd45bc496dea8a910ccd1d33c2b8f956
Gabriel Lins <nombregag@gmail.com>
2020-03-31 23:19:43 -0300

    remote db accessing

--- project/project/settings.py
+++ project/project/settings.py
@@ -80,11 +80,14 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'project',
+        'USER': 'project',
+        'PASSWORD': 'qazxsaq01',
+        'HOST': '64.227.29.85',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
